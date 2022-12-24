92bf809d26e5c4473e562cd6da75be70d8a54f1b
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-24 14:56:16 +0100

    Initial Commit

---
+++ CryptoCurrencyTracker/CryptoCurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyTrackerApp.swift
+//  CryptoCurrencyTracker
+//
+//  Created by Philipp Sanktjohanser on 24.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
